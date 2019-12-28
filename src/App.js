1d2baf9d15fa873d245516630284710f10eba2aa
Arthur <arthur.robieux@gmail.com>
2019-12-28 18:40:17 +0100

    Change App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Vive BaguetteTordue</div>
+        <img
+          className="Baguette"
+          src="http://a53.idata.over-blog.com/600x449/2/39/46/30/2010--juin-a-decembre-/09-Septembre-2010/CIMG5168.JPG"
+          alt="baguette"
+        />
       </header>
     </div>
   );
-}
+};
 
 export default App;
