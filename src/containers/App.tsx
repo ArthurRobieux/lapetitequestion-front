6c2567559560f7eeecb1ceefc88f9701a2a47625
Arthur <arthur.robieux@gmail.com>
2019-12-29 22:56:16 +0100

    Create tabs

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
 import { Menu } from "../modules/Menu";
-// import styles from "./styles.module.scss";
+import { HomePage } from "../modules/HomePage";
+import { Test } from "../modules/Test";
+
+import styles from "./styles.module.scss";
 
 export const App = withRouter(() => {
   return (
-    <div>
-      <div>Vive BaguetteTordue</div>
+    <div className={styles.app}>
+      <img
+        src={require("../assets/Mononoke.png")}
+        className={styles.logo}
+        alt="mononoke"
+      />
       <Menu />
+
+      <div className={styles.title}>Vive BaguetteTordue</div>
+
       <Switch>
-        <Route exact path="/" component={() => <div>Home</div>} />
-        <Route path="/1/" component={() => <div>1</div>} />
-        <Route path="/2/" component={() => <div>2</div>} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/test/" component={Test} />
       </Switch>
     </div>
   );
--- src/modules/Menu/Menu.tsx
+++ src/modules/Menu/Menu.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./styles.module.scss";
 
 export const Menu = () => {
   return (
-    <div>
-      <NavLink to="/">Go to Home</NavLink>
-      <NavLink to="/1/">Go to 1</NavLink>
-      <NavLink to="/2/">Go to 2</NavLink>
+    <div className={styles.menu}>
+      <NavLink className={styles.navlink} to="/">
+        Home
+      </NavLink>
+      <NavLink className={styles.navlink} to="/test/">
+        Test
+      </NavLink>
     </div>
   );
 };
