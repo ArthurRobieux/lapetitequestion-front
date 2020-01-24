21f734b0c0e7e4bf969305e002d0787142a6c1e3
Arthur <arthur.robieux@gmail.com>
2020-01-24 15:43:23 +0100

    Use ICONS

--- src/modules/home/HomePage/HomePage.tsx
+++ src/modules/home/HomePage/HomePage.tsx
@@ -6,11 +6,19 @@ export const HomePage = () => {
     <div>
       <div className={styles.title}>Test Specify</div>
 
+      <div className={styles.text}>Couleurs</div>
+
       <div className={styles.colorA}>Color A</div>
       <div className={styles.colorB}>Color B</div>
       <div className={styles.colorC}>Color C</div>
       <div className={styles.colorD}>Color D</div>
       <div className={styles.colorE}>Color E</div>
+
+      <div className={styles.text}>Icons</div>
+
+      <img src={require("../../assets/img/ICONS/MENU/CALENDAR/calendar.svg")} />
+      <img src={require("../../assets/img/ICONS/MENU/CALENDAR/collect.svg")} />
+      <img src={require("../../assets/img/ICONS/MENU/CALENDAR/members.svg")} />
     </div>
   );
 };
