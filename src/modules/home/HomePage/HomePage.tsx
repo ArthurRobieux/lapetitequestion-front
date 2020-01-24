38d6e0fd19cd43358cbb68d17425ff3fe0979509
Arthur <arthur.robieux@gmail.com>
2020-01-24 15:33:42 +0100

    Use Specify

--- src/modules/home/HomePage/HomePage.tsx
+++ src/modules/home/HomePage/HomePage.tsx
@@ -4,20 +4,13 @@ import styles from "./styles.module.scss";
 export const HomePage = () => {
   return (
     <div>
-      <div className={styles.title}>
-        Bienvenue sur le site de BaguetteTordue.
-      </div>
+      <div className={styles.title}>Test Specify</div>
 
-      <div className={styles.text}>
-        Le site est en construction. Mon premier projet est de réaliser un
-        escape game dans l'onglet prévu à cet effet.
-      </div>
-
-      <img
-        src={require("../../../assets/BaguetteTordue.jpg")}
-        alt="BaguetteTordue"
-        className={styles.image}
-      />
+      <div className={styles.colorA}>Color A</div>
+      <div className={styles.colorB}>Color B</div>
+      <div className={styles.colorC}>Color C</div>
+      <div className={styles.colorD}>Color D</div>
+      <div className={styles.colorE}>Color E</div>
     </div>
   );
 };
