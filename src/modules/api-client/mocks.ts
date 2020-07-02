cbfa991202519c0360b5a10eeb3d90e9ff4cec94
Arthur <arthur.robieux@gmail.com>
2020-07-02 10:52:17 +0200

    Improve mocks

--- src/modules/api-client/mocks.ts
+++ src/modules/api-client/mocks.ts
@@ -38,32 +38,35 @@ export type ApiPollResults = {
 // MOCKS
 
 export const poll_1: ApiPoll = {
-  title: "Mon sondage",
-  description: "Voici mon premier sondage",
+  title: "Equipement saison 2020-2021",
+  description: "Sondage pour les maillots de la prochaine saison",
   questions: [
     {
       id: 1,
-      description: "Vas tu te réinscrire l'année prochaine ?",
+      description: "Quel est ta taille de maillot ?",
       type: "single_choice",
       choices: [
-        { id: 1, description: "Oui" },
-        { id: 2, description: "Non" },
-        { id: 3, description: "Je ne sais pas" },
+        { id: 1, description: "XS" },
+        { id: 2, description: "S" },
+        { id: 3, description: "M" },
+        { id: 4, description: "L" },
+        { id: 5, description: "XL" },
       ],
     },
     {
       id: 2,
-      description: "Qu'a tu préféré cette année dans ton club?",
+      description: "Quelles sont tes marques préférées ?",
       type: "multiple_choices",
       choices: [
-        { id: 1, description: "La compèt" },
-        { id: 2, description: "Les potes" },
-        { id: 3, description: "Les barbecue" },
+        { id: 1, description: "Nike" },
+        { id: 2, description: "Adidas" },
+        { id: 3, description: "Puma" },
+        { id: 4, description: "Kipsta" },
       ],
     },
     {
       id: 3,
-      description: "Quelque chose à ajouter ?",
+      description: "Quel numéro veux tu sur ton maillot ?",
       type: "text",
       choices: [],
     },
@@ -89,49 +92,166 @@ export const poll_2: ApiPoll = {
 };
 
 export const poll_results_1: ApiPollResults = {
-  title: "Mon sondage",
-  description: "Voici mon premier sondage",
+  title: "Equipement saison 2020-2021",
+  description: "Sondage pour les maillots de la prochaine saison",
   questions: [
     {
       id: 1,
-      description: "Vas tu te réinscrire l'année prochaine ?",
+      description: "Quel est ta taille de maillot ?",
       type: "single_choice",
       answers: [
-        { name: "Arthur", choices: [{ id: 1, description: "Oui" }] },
-        { name: "Julie", choices: [{ id: 1, description: "Oui" }] },
-        { name: "Lénaic", choices: [{ id: 2, description: "Non" }] },
+        { name: "Arthur", choices: [{ id: 3, description: "M" }] },
+        { name: "Julie", choices: [{ id: 3, description: "M" }] },
+        { name: "Lénaic", choices: [{ id: 3, description: "M" }] },
+        { name: "Quentin", choices: [{ id: 3, description: "M" }] },
+        { name: "Matéo", choices: [{ id: 3, description: "M" }] },
+        { name: "Andy", choices: [{ id: 1, description: "XS" }] },
+        { name: "Thomas", choices: [{ id: 3, description: "M" }] },
+        { name: "Vincent", choices: [{ id: 3, description: "M" }] },
+        { name: "Esselinho", choices: [{ id: 3, description: "M" }] },
+        { name: "Karl", choices: [{ id: 3, description: "M" }] },
+        { name: "Julien", choices: [{ id: 3, description: "M" }] },
+        { name: "Nizar", choices: [{ id: 5, description: "XL" }] },
+        { name: "Albin", choices: [{ id: 3, description: "M" }] },
+        { name: "Anne-lise", choices: [{ id: 3, description: "M" }] },
+        { name: "Florian", choices: [{ id: 3, description: "M" }] },
+        { name: "Harold", choices: [{ id: 3, description: "M" }] },
+        { name: "Guillaume", choices: [{ id: 3, description: "M" }] },
+        { name: "Mathis", choices: [{ id: 3, description: "M" }] },
+        { name: "Etienne", choices: [{ id: 5, description: "XL" }] },
       ],
     },
     {
       id: 2,
-      description: "Qu'a tu préféré cette année dans ton club?",
+      description: "Quelles sont tes marques préférées ?",
       type: "multiple_choices",
       answers: [
         {
           name: "Arthur",
+          choices: [{ id: 1, description: "Nike" }],
+        },
+        {
+          name: "Julie",
           choices: [
-            { id: 1, description: "La compèt" },
-            { id: 2, description: "Les potes" },
+            { id: 1, description: "Nike" },
+            { id: 4, description: "Kipsta" },
           ],
         },
-        { name: "Julie", choices: [{ id: 3, description: "Les barbecue" }] },
+        { name: "Lénaïc", choices: [{ id: 2, description: "Adidas" }] },
         {
-          name: "Lénaic",
+          name: "Quentin",
           choices: [
-            { id: 2, description: "Les potes" },
-            { id: 3, description: "Les barbecue" },
+            { id: 3, description: "Puma" },
+            { id: 4, description: "Kipsta" },
           ],
+        },
+        {
+          name: "Matéo",
+          choices: [{ id: 1, description: "Nike" }],
+        },
+        {
+          name: "Andy",
+          choices: [
+            { id: 1, description: "Nike" },
+            { id: 3, description: "Puma" },
+          ],
+        },
+        {
+          name: "Thomas",
+          choices: [{ id: 2, description: "Adidas" }],
+        },
+        {
+          name: "Vincent",
+          choices: [
+            { id: 1, description: "Nike" },
+            { id: 4, description: "Kipsta" },
+          ],
+        },
+        {
+          name: "Esselinho",
+          choices: [
+            { id: 2, description: "Adidas" },
+            { id: 3, description: "Puma" },
+          ],
+        },
+        {
+          name: "Karl",
+          choices: [{ id: 1, description: "Nike" }],
+        },
+        {
+          name: "Julien",
+          choices: [{ id: 4, description: "Kipsta" }],
+        },
+        {
+          name: "Nizar",
+          choices: [
+            { id: 2, description: "Adidas" },
+            { id: 3, description: "Puma" },
+          ],
+        },
+        {
+          name: "Albin",
+          choices: [
+            { id: 1, description: "Nike" },
+            { id: 2, description: "Adidas" },
+          ],
+        },
+        {
+          name: "Anne-lise",
+          choices: [{ id: 1, description: "Nike" }],
+        },
+        {
+          name: "Florian",
+          choices: [
+            { id: 2, description: "Adidas" },
+            { id: 3, description: "Puma" },
+          ],
+        },
+        {
+          name: "Harold",
+          choices: [{ id: 3, description: "Puma" }],
+        },
+        {
+          name: "Guillaume",
+          choices: [{ id: 4, description: "Kipsta" }],
+        },
+        {
+          name: "Mathis",
+          choices: [
+            { id: 1, description: "Nike" },
+            { id: 3, description: "Puma" },
+          ],
+        },
+        {
+          name: "Etienne",
+          choices: [{ id: 2, description: "Adidas" }],
         },
       ],
     },
     {
       id: 3,
-      description: "Quelque chose à ajouter ?",
+      description: "Quel numéro veux tu sur ton maillot ?",
       type: "text",
       answers: [
-        { name: "Arthur", text: "RAS" },
-        { name: "Julie", text: "Le sondage était top" },
-        { name: "Lénaic", text: "Je vais quitter le club " },
+        { name: "Arthur", text: "23" },
+        { name: "Julie", text: "21" },
+        { name: "Lénaic", text: "7" },
+        { name: "Quentin", text: "15" },
+        { name: "Matéo", text: "19" },
+        { name: "Andy", text: "2" },
+        { name: "Thomas", text: "11" },
+        { name: "Vincent", text: "25" },
+        { name: "Esselinho", text: "4" },
+        { name: "Karl", text: "12" },
+        { name: "Julien", text: "42" },
+        { name: "Nizar", text: "1" },
+        { name: "Albin", text: "6" },
+        { name: "Anne-lise", text: "18" },
+        { name: "Florian", text: "17" },
+        { name: "Harold", text: "5" },
+        { name: "Guillame", text: "8" },
+        { name: "Mathis", text: "10" },
+        { name: "Etienne", text: "13" },
       ],
     },
   ],
